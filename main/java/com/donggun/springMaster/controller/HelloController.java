a3fdbc23ca81fc1f789728ada656b1da4c64026e
Donggun.Chung <chungdk1117@gmail.com>
2021-04-16 23:18:06 +0900

    2021.04.16. Fri. PathVariable, RequestParam Annotation Test

--- main/java/com/donggun/springMaster/controller/HelloController.java
+++ main/java/com/donggun/springMaster/controller/HelloController.java
@@ -1,15 +1,38 @@
 package com.donggun.springMaster.controller;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 	
-	@RequestMapping("/hello.do")
+	@RequestMapping(value= {"/main", "/index"})
 	public String hello(Model model) {
 		model.addAttribute("greeting", "Hello everyone!");
 		return "hello";
+	}
+	
+	// @PathVariable Test
+	@RequestMapping("/multiple/{number}")
+	public String multipleTen(@PathVariable("number") int number, Model model) {
+		model.addAttribute("multipleTen", number * 10);
+		return "multiple";
+	}
+	
+	// @RequestParam Test
+	@RequestMapping("/detail")
+	public String detail(@RequestParam(value="id", required=true, defaultValue="admin") String requestParam, Model model) throws IOException {
+		if(requestParam == null) {
+			return "redirect:/";
+		}
+		
+		model.addAttribute("detailInfo", "This is Detail Information");
+		model.addAttribute("requestParam", requestParam);
+		return "detail";
 	}
 }
