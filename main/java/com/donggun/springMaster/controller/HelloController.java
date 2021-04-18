403ffb5c97b3c1990f9f28b5180df55b46c7eea2
Donggun.Chung <chungdk1117@gmail.com>
2021-04-18 17:23:29 +0900

    2021.04.18. Sun. Controller bean 제거 및 detail 요청 제거

--- main/java/com/donggun/springMaster/controller/HelloController.java
+++ main/java/com/donggun/springMaster/controller/HelloController.java
@@ -1,12 +1,9 @@
 package com.donggun.springMaster.controller;
 
-import java.io.IOException;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller("Hello")
 public class HelloController {
@@ -22,18 +19,6 @@ public class HelloController {
 	public String multipleTen(@PathVariable("number") int number, Model model) {
 		model.addAttribute("multipleTen", number * 10);
 		return "multiple";
-	}
-	
-	// @RequestParam Test
-	@RequestMapping("/detail")
-	public String detail(@RequestParam(value="id", required=true, defaultValue="admin") String requestParam, Model model) throws IOException {
-		if(requestParam == null) {
-			return "redirect:/";
-		}
-		
-		model.addAttribute("detailInfo", "This is Detail Information");
-		model.addAttribute("requestParam", requestParam);
-		return "detail";
 	}
 	
 }
