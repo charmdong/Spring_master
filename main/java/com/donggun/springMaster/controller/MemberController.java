89064be574f33687e5e4e0bf4804a5036da4e81b
Donggun.Chung <chungdk1117@gmail.com>
2021-04-19 20:26:13 +0900

    2021.04.19. Mon. MemberVO 커맨드 객체 값 검증 적용

--- main/java/com/donggun/springMaster/controller/MemberController.java
+++ main/java/com/donggun/springMaster/controller/MemberController.java
@@ -2,10 +2,13 @@ package com.donggun.springMaster.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.donggun.springMaster.dto.MemberVO;
+import com.donggun.springMaster.validator.MemberVOValidator;
 
 /**
  * @author donggun.Chung
@@ -36,9 +39,23 @@ public class MemberController {
 		return "member/join";
 	}
 	
-	// 커맨드 객체 Test 
+	/**
+	 * 커맨드 객체 Test
+	 * 전달인자로 커맨드 객체 뒤에 바로 Errors나 BindingResult가 위치해야 한다.
+	 * 
+	 * @param member
+	 * @param errors
+	 */
 	@RequestMapping("/regist")
-	public String memberRegist(MemberVO member) {
+	public String memberRegist(@ModelAttribute("memberInfo") MemberVO member, Errors errors) {
+		// 커맨드 객체 값 검증
+		new MemberVOValidator().validate(member, errors);
+		if(errors.hasErrors()) {
+			return "member/join";
+		}
+		
+		// TODO Service BL
+		
 		return "member/detail";
 	}
 }
