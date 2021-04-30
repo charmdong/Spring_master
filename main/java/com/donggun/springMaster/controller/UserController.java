352b6cb6ec91ea65f59880708c6aad87c6a7999f
Donggun.Chung <chungdk1117@gmail.com>
2021-04-30 21:57:24 +0900

    :hammer: 2021.04.30. Fri. [로그인, 로그아웃 요청 처리 메소드 생성]

--- main/java/com/donggun/springMaster/controller/UserController.java
+++ main/java/com/donggun/springMaster/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.donggun.springMaster.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.donggun.springMaster.vo.UserVO;
 
 /**
  * User Controller
@@ -12,5 +18,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 @Controller
 public class UserController {
+	
+	/**
+	 * 로그인 페이지로 이동
+	 * @param 
+	 * @return loginForm.jsp
+	 */
+	@RequestMapping(value="/loginForm", method=RequestMethod.GET)
+	public String loginForm() {
+		return "loginForm";
+	}
+	
+	/**
+	 * 로그인
+	 * @param user
+	 * @return main.jsp
+	 */
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String login(@RequestBody UserVO user) {
+		// TOOD login BL
+		
+		return "main";
+	}
+	
+	/**
+	 * 로그아웃
+	 * @param session
+	 * @return index.jsp 
+	 */
+	@RequestMapping(value="/logout", method=RequestMethod.GET) 
+	public String logout(HttpSession session) {
+		session.invalidate();
+		
+		return "index";
+	}
 	
 }
