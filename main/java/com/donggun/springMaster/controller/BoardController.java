f3d19b7e5db4a084e0403df353156bdb75859855
Donggun.Chung <chungdk1117@gmail.com>
2021-05-01 12:34:40 +0900

    :hammer: 2021.05.01. Sat. [게시글 목록, 상세 조회 요청 처리 메소드 추가]

--- main/java/com/donggun/springMaster/controller/BoardController.java
+++ main/java/com/donggun/springMaster/controller/BoardController.java
@@ -1,7 +1,11 @@
 package com.donggun.springMaster.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Board Controller
@@ -13,4 +17,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BoardController {
 
+	/**
+	 * 전달된 아이디에 해당하는 사용자의 게시물 목록 조회
+	 * @param id
+	 * @param model
+	 * @return list.jsp
+	 */
+	@RequestMapping(value="/list", method=RequestMethod.GET)
+	public String getBoardList(@RequestParam("id") String id, Model model) {
+		// List<BoardVO> boardList = boardService.getBoardList();
+		// model.addAttribute("boardList", boardList);
+		
+		return "board/list";
+	}
+	
+	/**
+	 * 게시물 상세 조회 
+	 * @param boardNo
+	 * @param model
+	 * @return detail.jsp
+	 */
+	@RequestMapping(value="/detail/{boardNo}", method=RequestMethod.GET)
+	public String getDetailBoard(@PathVariable String boardNo, Model model) {
+		// BoardVO board = boardService.getDetailBoard(boardNo);
+		// model.addAttribute("board", board);
+		
+		return "board/detail";
+	}
+	
 }
