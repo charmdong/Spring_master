5b295d57911f6fed48b1b97ee36615873ee387d3
Donggun.Chung <chungdk1117@gmail.com>
2021-05-01 13:22:13 +0900

    :hammer: 2021.05.01. Sat. [게시물 수정 및 삭제 요청 처리 메소드 추가]

--- main/java/com/donggun/springMaster/controller/BoardController.java
+++ main/java/com/donggun/springMaster/controller/BoardController.java
@@ -2,10 +2,14 @@ package com.donggun.springMaster.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.donggun.springMaster.vo.BoardVO;
 
 /**
  * Board Controller
@@ -41,8 +45,43 @@ public class BoardController {
 	public String getDetailBoard(@PathVariable String boardNo, Model model) {
 		// BoardVO board = boardService.getDetailBoard(boardNo);
 		// model.addAttribute("board", board);
+		// List<CommentVO> commentList = commentService.getCommentList(boardNo);
+		// model.addAttribute("commentList", commentList);
 		
 		return "board/detail";
 	}
 	
+	/**
+	 * 게시물 삭제
+	 * @param boardNo
+	 * @return list.jsp
+	 */
+	@RequestMapping(value="/delete", method=RequestMethod.DELETE)
+	public String deleteBoard(@RequestParam String boardNo) {
+		// boardService.deleteBoard(boardNo);
+		
+		return "redirect:/list";
+	}
+	
+	/**
+	 * 수정 폼 이동
+	 * @param board
+	 * @return modifyForm.jsp
+	 */
+	@RequestMapping(value="/modifyForm", method=RequestMethod.POST)
+	public String modifyForm(@ModelAttribute("board") BoardVO board) {
+		return "board/modifyForm";
+	}
+	
+	/**
+	 * 게시물 수정
+	 * @param board
+	 * @return list.jsp
+	 */
+	@RequestMapping(value="/modify", method=RequestMethod.PUT)
+	public String modifyBoard(@RequestBody BoardVO board) {
+		// boardService.modifyBoard(board);
+		
+		return "redirect:/list";
+	}
 }
