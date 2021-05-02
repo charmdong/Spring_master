8e346f92a7ee13bd2edac05952278b81c0e8bb2b
Donggun.Chung <chungdk1117@gmail.com>
2021-05-02 22:51:30 +0900

    :wrench: 2021.05.02. Sun. [BoardService 및 impl 생성]

---
+++ main/java/com/donggun/springMaster/Service/BoardService.java
@@ -0,0 +1,11 @@
+package com.donggun.springMaster.service;
+
+/**
+ * Board Service Interface
+ * @author donggun.Chung
+ * @since 2021.05.02.
+ * @version 1.0
+ */
+public interface BoardService {
+
+}
---
+++ main/java/com/donggun/springMaster/Service/impl/BoardServiceImpl.java
@@ -0,0 +1,17 @@
+package com.donggun.springMaster.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.donggun.springMaster.service.BoardService;
+
+/**
+ * Board 서비스 구현 객체
+ * @author donggun.Chung
+ * @since 2021.05.02.
+ * @version 1.0 
+ */
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+}
