20b4d884d4c2bf3070a5c6d96a4f24ebaf2bc2cf
Donggun.Chung <chungdk1117@gmail.com>
2021-05-27 22:15:53 +0900

    :hammer: 2021.05.27. Thu. [BoardVO 생성 및 CRUD 메서드 추가]

---
+++ main/java/com/donggun/springMaster/dao/BoardDAO.java
@@ -0,0 +1,57 @@
+package com.donggun.springMaster.dao;
+
+import java.util.List;
+
+import com.donggun.springMaster.vo.BoardVO;
+
+/**
+ * Board DAO
+ * @author donggun.Chung
+ * @since 2021.05.27.
+ * @version 1.0
+ */
+public class BoardDAO {
+
+	/**
+	 * 게시글 조회
+	 * @param boardNo
+	 * @return board
+	 */
+	public BoardVO getBoard(String boardNo) {
+		return null;
+	}
+	
+	/**
+	 * 사용자 작성 게시글 목록 조회
+	 * @param id
+	 * @return
+	 */
+	public List<BoardVO> getBoardList(String id) {
+		return null;
+	}
+	
+	/**
+	 * 게시글 등록
+	 * @param board
+	 */
+	public void reigstBoard(BoardVO board) {
+		;
+	}
+	
+	/**
+	 * 게시글 수정
+	 * @param boardNo
+	 * @param board
+	 */
+	public void modifyBoard(String boardNo, BoardVO board) {
+		;
+	}
+	
+	/**
+	 * 게시글 삭제
+	 * @param boardNo
+	 */
+	public void deleteBoard(String boardNo) {
+		;
+	}
+}
