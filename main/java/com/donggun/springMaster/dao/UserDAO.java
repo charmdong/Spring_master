b50faa4e5e2891dfbf9a7dbe1bf651875d40f433
Donggun.Chung <chungdk1117@gmail.com>
2021-06-01 20:54:09 +0900

    :hammer: 2021.06.01. Tue. [EntityManager 의존성 주입 및 로직 구현]

--- main/java/com/donggun/springMaster/dao/UserDAO.java
+++ main/java/com/donggun/springMaster/dao/UserDAO.java
@@ -1,5 +1,10 @@
 package com.donggun.springMaster.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
 
 import com.donggun.springMaster.vo.UserVO;
@@ -13,6 +18,9 @@ import com.donggun.springMaster.vo.UserVO;
 @Repository
 public class UserDAO {
 	
+	@PersistenceContext
+	EntityManager em;
+	
 	/**
 	 * 사용자 정보 조회
 	 * @param id
@@ -20,7 +28,16 @@ public class UserDAO {
 	 * @throws Exception
 	 */
 	public UserVO getUserInfo(String id) throws Exception {
-		return null;
+		return em.find(UserVO.class, id);
+	}
+	
+	/**
+	 * 모든 사용자 정보 목록 조회
+	 * @return
+	 * @throws Exception
+	 */
+	public List<UserVO> getAllUserList() throws Exception {
+		return em.createQuery("select u from USER u", UserVO.class).getResultList();
 	}
 	
 	/**
@@ -29,7 +46,7 @@ public class UserDAO {
 	 * @throws Exception
 	 */
 	public void registUserInfo(UserVO userInfo) throws Exception {
-		;
+		em.persist(userInfo);
 	}
 	
 	/**
@@ -39,7 +56,9 @@ public class UserDAO {
 	 * @throws Exception
 	 */
 	public void modifyUserInfo(UserVO userInfo) throws Exception {
-		;
+		UserVO user = em.find(UserVO.class, userInfo.getId());
+		
+		// TODO user 정보 수정
 	}
 
 	/**
@@ -48,6 +67,8 @@ public class UserDAO {
 	 * @throws Exception
 	 */
 	public void deleteUserInfo(String id) throws Exception {
-		;
+		UserVO user = em.find(UserVO.class, id);
+		
+		em.remove(user);
 	}
 }
