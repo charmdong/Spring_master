22155b7939af3dacb337a85f6f2a7163e89c6f16
Donggun.Chung <chungdk1117@gmail.com>
2021-06-16 15:40:01 +0900

    :hammer: 2021.06.16. Wed. [make FileVO to Entity]

--- main/java/com/donggun/springMaster/vo/FileVO.java
+++ main/java/com/donggun/springMaster/vo/FileVO.java
@@ -1,47 +1,81 @@
 package com.donggun.springMaster.vo;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
 /**
  * 이미지 파일 정보 VO
+ * 
  * @author donggun.Chung
  * @since 2021.04.27.
  * @version 1.0
  */
+@Entity
+@Table(name = "FILE")
 public class FileVO {
-	
-	@Id @GeneratedValue(strategy=GenerationType.IDENTITY) private String fileNo;
-	private String boardNo;		// FK
-	private String fileName;	// CPK with boardNo
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String fileNo;
+
+	@ManyToOne
+	@JoinColumn(name = "BOARD_NO")
+	private BoardVO board;
+
+	private String fileName; // CPK with boardNo
 	private String filePath;
-	@Size(max=1024000) private String fileSize;
-	
-	public String getboardNo() {
-		return boardNo;
+	@Size(max = 1024000)
+	private String fileSize;
+
+	public String getFileNo() {
+		return fileNo;
 	}
-	public void setboardNo(String boardNo) {
-		this.boardNo = boardNo;
+
+	public void setFileNo(String fileNo) {
+		this.fileNo = fileNo;
 	}
+
+	public BoardVO getBoard() {
+		return board;
+	}
+
+	public void setBoard(BoardVO board) {
+		if (this.board != null) {
+			this.board.getFileList().remove(this);
+		}
+
+		this.board = board;
+		board.getFileList().add(this);
+	}
+
 	public String getFileName() {
 		return fileName;
 	}
+
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
+
 	public String getFilePath() {
 		return filePath;
 	}
+
 	public void setFilePath(String filePath) {
 		this.filePath = filePath;
 	}
+
 	public String getFileSize() {
 		return fileSize;
 	}
+
 	public void setFileSize(String fileSize) {
 		this.fileSize = fileSize;
 	}
-	
+
 }
