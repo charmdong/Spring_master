70eef7d29d96ad9681606cd1c51330758b39bf2b
Donggun.Chung <chungdk1117@gmail.com>
2021-06-18 23:09:16 +0900

    :hammer: 2021.06.18. Fri. [add database test]

---
+++ test/java/springMaster/DBConnectionTest.java
@@ -0,0 +1,23 @@
+package springMaster;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.junit.Test;
+
+public class DBConnectionTest {
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost:3306/spring_master?useSSL=false&serverTimezone=UTC";
+	private static final String USER = "donggun";
+	private static final String PW = "donggun";
+
+	@Test
+	public void testConnection() throws Exception{
+		Class.forName(DRIVER);
+		try (Connection con = DriverManager.getConnection(URL, USER, PW))	{
+			System.out.println(con);
+		}catch(Exception e){
+			System.err.println(e);
+		}
+	}
+
+}
