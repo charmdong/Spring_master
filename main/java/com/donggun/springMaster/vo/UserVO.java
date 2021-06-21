5bfb92e4ee331db738be8fde457310a2f4fb6b7c
Donggun.Chung <chungdk1117@gmail.com>
2021-06-21 23:26:48 +0900

    :hammer: 2021.06.21.Mon [add @Column name]

--- main/java/com/donggun/springMaster/vo/UserVO.java
+++ main/java/com/donggun/springMaster/vo/UserVO.java
@@ -20,110 +20,154 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * 사용자 정보 VO
+ * 
  * @author donggun.Chung
  * @since 2021.04.27.
  * @version 1.0
  */
 
 @Entity
-@Table(name="USER")
+@Table(name = "USER")
 public class UserVO {
-	
-	@Id @NotNull @NotEmpty private String id;
-	@NotNull @NotEmpty private String nickName;
-	@NotNull @NotEmpty private String password;
-	@NotNull @NotEmpty private String userName;
-	@Email private String email;
-	@NotNull @NotEmpty private String phone;
+
+	@Id
+	@NotNull
+	@NotEmpty
+	private String id;
+	@NotNull
+	@NotEmpty
+	@Column(name="NICK_NAME")
+	private String nickName;
+	@NotNull
+	@NotEmpty
+	private String password;
+	@NotNull
+	@NotEmpty
+	@Column(name="USER_NAME")
+	private String userName;
+	@Email
+	private String email;
+	@NotNull
+	@NotEmpty
+	private String phone;
 	private String homepage;
 	private String description;
-	
-	@OneToMany(mappedBy="user")
+
+	@OneToMany(mappedBy = "user")
 	private List<BoardVO> boardList = new ArrayList<BoardVO>();
-	
-	@NotNull @NotEmpty
+
+	@NotNull
+	@NotEmpty
+	@Column(name="ROLE_TYPE")
 	@Enumerated(EnumType.STRING)
 	private RoleType roleType;
-	
-	@NotNull @NotEmpty
+
+	@NotNull
+	@NotEmpty
+	@Column(name="REG_DATE")
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date createdDate;
-	
-	@NotNull @NotEmpty
+	private Date regDate;
+
+	@NotNull
+	@NotEmpty
+	@Column(name="MOD_DATE")
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date modifiedDate;
-	
+	private Date modDate;
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getNickName() {
 		return nickName;
 	}
+
 	public void setNickName(String nickName) {
 		this.nickName = nickName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getHomepage() {
 		return homepage;
 	}
+
 	public void setHomepage(String homepage) {
 		this.homepage = homepage;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public RoleType getRoleType() {
 		return roleType;
 	}
+
 	public void setRoleType(RoleType roleType) {
 		this.roleType = roleType;
 	}
-	public Date getCreatedDate() {
-		return createdDate;
+
+	public Date getRegDate() {
+		return regDate;
 	}
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
 	}
-	public Date getModifiedDate() {
-		return modifiedDate;
+
+	public Date getModDate() {
+		return modDate;
 	}
-	public void setModifiedDate(Date modifiedDate) {
-		this.modifiedDate = modifiedDate;
+
+	public void setModDate(Date modDate) {
+		this.modDate = modDate;
 	}
+
 	public List<BoardVO> getBoardList() {
 		return boardList;
 	}
+
 	public void setBoardList(List<BoardVO> boardList) {
 		this.boardList = boardList;
 	}
-	
+
 }
