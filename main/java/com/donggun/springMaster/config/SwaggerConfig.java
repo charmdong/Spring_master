383df8e8e27cc6c235e4a4b199becb8f391e747c
Donggun.Chung <chungdk1117@gmail.com>
2021-07-08 22:11:35 +0900

    :wrench: 2021.07.08.Thu [add apiInfo]

--- main/java/com/donggun/springMaster/config/SwaggerConfig.java
+++ main/java/com/donggun/springMaster/config/SwaggerConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +20,7 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.donggun.springMaster.controller.RestController"))
 				.paths(PathSelectors.any())
@@ -25,6 +28,14 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
 				.pathMapping("/");
 	}
 
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Spring Master")
+				.version("1.0.0-SNAPSHOT")
+				.description("Spring Master API")
+				.build();
+	}
+	
 	// Resource Handler에 Swagger UI 등록하기 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
